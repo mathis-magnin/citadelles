4ebd6ad4d83bb29a8a2ee802eba074335e666317
Heschung Erwan <erwan.heschung@etu.univ-cotedazur.fr>
2023-12-08 15:58:45 +0100

    feat: ajout de l'éxecutable #6

---
+++ src/main/java/fr/citadels/Main.java
@@ -0,0 +1,12 @@
+package fr.citadels;
+
+import fr.citadels.engine.Game;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Game myGame = new Game();
+        myGame.playGame();
+    }
+
+}
